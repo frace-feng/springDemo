625139beeb7c7f8765c10de9270fd07e0052763b
FRACE <frace@inmoods.com>
2019-10-25 17:17:23 +0800

    hibernate 机制

---
+++ myHibernate/src/main/java/com/frace/App.java
@@ -0,0 +1,13 @@
+package com.frace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ myHibernate/src/main/java/org/hibernate/cache/internal/NoCacheProvider.java
@@ -0,0 +1,7 @@
+package org.hibernate.cache.internal;
+
+/**
+ * Created by frace on 2019/10/25 17:09
+ */
+public class NoCacheProvider {
+}
